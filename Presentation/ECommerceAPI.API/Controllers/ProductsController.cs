31634e25722f0c9c09c8531d53a77d2c65b1c26e
mehmettgunerr <63156838+mehmettgunerr@users.noreply.github.com>
2022-03-14 01:19:57 +0300

    Add project files.

---
+++ Presentation/ECommerceAPI.API/Controllers/ProductsController.cs
@@ -0,0 +1,45 @@
+﻿using ECommerceAPI.Application.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductReadRepository _productReadRepository;
+        private readonly IProductWriteRepository _productWriteRepository;
+
+        public ProductsController(IProductReadRepository productReadRepository, IProductWriteRepository productWriteRepository)
+        {
+            _productReadRepository = productReadRepository;
+            _productWriteRepository = productWriteRepository;
+        }
+
+        [HttpGet]
+        public async Task Get()
+        {
+            //await _productWriteRepository.AddRangeAsync(new()
+            //{
+            //    new() { Id = Guid.NewGuid(), Name = "Product 1", Price = 100, CreatedDate = DateTime.UtcNow, Stock = 10 },
+            //    new() { Id = Guid.NewGuid(), Name = "Product 2", Price = 200, CreatedDate = DateTime.UtcNow, Stock = 20 },
+            //    new() { Id = Guid.NewGuid(), Name = "Product 3", Price = 300, CreatedDate = DateTime.UtcNow, Stock = 30 }
+            //});
+
+            //await _productWriteRepository.SaveAsync();
+
+            var p = await _productReadRepository.GetByIdAsync("e8a27c57-d5b0-4bd2-b2d3-6a0b55b0cd74");
+            p.Name = "Mehmet";
+
+            await _productWriteRepository.SaveAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            var product = await _productReadRepository.GetByIdAsync(id);
+            return Ok(product);
+        }
+    }
+}
